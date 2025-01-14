905577d8e72725dd0b6c59e66f3d52e8f195b4c3
atrox3d <atrox3d@gmail.com>
2025-01-14 12:34:27 +0100

    format, add doc

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import repos
 d = {}
 root = '~/code'
 
-for repo in repos.scan(root, True):
+for repo in repos.scan(root, relative=True):
     d = repos.add_to_dict(repo, d)
-    repos.add_remote(repo, root, d)
+    repos.add_remote(repo, d, root)
 
 with open('repos.json', 'w') as fp:
     json.dump(d, fp, indent=2)
--- repos.py
+++ repos.py
@@ -1,34 +1,59 @@
+from operator import add
 from pathlib import Path
 from atrox3d import simplegit
 
     
-def scan(start_path:str, relative:bool=True) -> list[Path]:
+def scan(root:str, exclude:tuple=('.venv',), additional_excludes:list=None, relative:bool=True) -> list[Path]:
+    '''
+    scan root path and returns a list of repo paths
     
-    start = Path(start_path).expanduser()
-    assert start.exists(), f'path {start_path} does not exist'
+    if relative is True the paths start relative to root
     
+    exclude and additional_excludes are joined and used to check which paths to exclude
+    '''
+    
+    # expand ~ and check if valid path
+    start = Path(root).expanduser()
+    assert start.exists(), f'path {root} does not exist'
+    
+    # create exclude list
+    additional_excludes = [] if additional_excludes is None else additional_excludes
+    exclude_paths = [*exclude, *additional_excludes]
+    
+    # scan all git repos
     repos = start.glob('**/.git/')
-    repo_dirs =  [d.parent for d in repos if d.is_dir() and '.venv' not in d.parts]
+    repo_dirs =  [
+            d.parent for d in repos                     # save parent
+            if d.is_dir()                               # if d is a dir
+            and not any(                                # and does not contain
+                p in exclude_paths for p in d.parts     # any of excluded names
+            )
+    ]
     
+    # cut the root part if relative
     if relative:
         repo_dirs = [Path(d).relative_to(start) for d in repo_dirs]
     
     return repo_dirs
 
 
-def add_to_dict(path:Path, d:dict=None) -> dict:
+def add_to_dict(path:Path, d:dict) -> dict:
+    ''' create or add paths to dice as keys:dict with the last being {} '''
     
+    # create dict if not passed
     d = d if d is not None else {}
-    c = d
+    
+    cursor = d
 
     for part in path.parts:
-        c[part] = c.get(part, {})
-        c = c[part]
+        cursor[part] = cursor.get(part, {})
+        cursor = cursor[part]
     
     return d
 
 
-def add_remote(path:Path, root:str=None, d:dict=None) -> dict:
+def add_remote(path:Path, d:dict, root:str=None) -> dict:
+    ''' add git repo remote to the last key or None '''
     
     if root is not None:
         git_path = Path(root).expanduser() / path
@@ -39,3 +64,4 @@ def add_remote(path:Path, root:str=None, d:dict=None) -> dict:
     for part in parents:
         d = d[part]
     d[repo] = remote
+
