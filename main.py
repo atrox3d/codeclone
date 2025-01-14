96750b1e602782386bb7c95a88a16034b5e829e4
atrox3d <atrox3d@gmail.com>
2025-01-14 11:17:57 +0100

    add to_dict and add_remote

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import json
 import repos
 
-for repo in repos.scan('~/code', True):
-    print(repo)
+d = {}
+root = '~/code'
+for repo in repos.scan(root, True):
+    d = repos.add_to_dict(repo, d)
+    # print(json.dumps(d, indent=2))
+    print('MAIN', repo)
+    repos.add_remote(repo, root, d)
+    # print(repo, repos.add_remote(repo, root))
+
+print(json.dumps(d, indent=2))
--- repos.py
+++ repos.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from atrox3d import simplegit
 
     
 def scan(start_path:str, relative:bool=True) -> list[Path]:
@@ -7,9 +8,35 @@ def scan(start_path:str, relative:bool=True) -> list[Path]:
     assert start.exists(), f'path {start_path} does not exist'
     
     repos = start.glob('**/.git/')
-    repo_dirs =  [d for d in repos if d.is_dir()]
+    repo_dirs =  [d.parent for d in repos if d.is_dir() and '.venv' not in d.parts]
     
     if relative:
         repo_dirs = [Path(d).relative_to(start) for d in repo_dirs]
     
     return repo_dirs
+
+
+def add_to_dict(path:Path, d:dict=None) -> dict:
+    d = d if d is not None else {}
+    c = d
+
+    for part in path.parts:
+        c[part] = c.get(part, {})
+        c = c[part]
+    
+    return d
+
+def add_remote(path:Path, root:str=None, d:dict=None) -> dict:
+    if root is not None:
+        gitpath = Path(root).expanduser() / path
+    # print('ADD_REMOTE', path)
+    remote = simplegit.git.get_remote(gitpath)
+    
+    print(path)
+    for part in path.parts[:-1]:
+        d = d[part]
+    
+    print(path.parts, d)
+    print(d[path.parts[-1]])
+    d[path.parts[-1]] = remote
+
